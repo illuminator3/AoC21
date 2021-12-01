61024d7cbca79ec7191670410219ae8021347f70
illuminator3 <hardt-j@web.de>
2021-12-01 15:54:57 +0100

    Add universal solve function

--- Days/Day1.hs
+++ Days/Day1.hs
@@ -9,23 +9,19 @@ solveX :: Show a => ([Int] -> a) -> IO ()
 solveX f = getInput >>= putStrLn . show . f . map readInt . lines
 
 solveDay1Part1 :: IO ()
-solveDay1Part1 = solveX solve1
-
-solve1 :: (Num a, Ord a) => [a] -> a
-solve1 [] = 0
-solve1 (x:xs) = solve1' x xs 0
-
-solve1' :: (Num a, Ord a) => a -> [a] -> a -> a
-solve1' _ [] = id
-solve1' c (x:xs) = solve1' x xs . (+) (if x > c then 1 else 0)
+solveDay1Part1 = solveX solve
 
 solveDay1Part2 :: IO ()
 solveDay1Part2 = solveX solve2
 
+solve :: (Num a, Ord a) => [a] -> a
+solve [] = 0
+solve (x:xs) = solve' x xs 0
+
+solve' :: (Num a, Ord a) => a -> [a] -> a -> a
+solve' _ [] = id
+solve' c (x:xs) = solve' x xs . (+) (if x > c then 1 else 0)
+
 solve2 :: (Num a, Ord a) => [a] -> a
 solve2 [] = 0
-solve2 xs = solve2' (map sum . groupOf 3 $ xs)
-
-solve2' :: (Num a, Ord a) => [a] -> a
-solve2' [] = 0
-solve2' (x:xs) = solve1' x xs 0 
+solve2 xs = solve (map sum . groupOf 3 $ xs)
